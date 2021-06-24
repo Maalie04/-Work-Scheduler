891ec5bdb59e42c2b0711998b9817b170142cd30
Jamaal Lawson <maaliemaal@gmail.com>
2021-06-24 18:16:03 -0400

    added a event listener to my button inorder to save different inputs

--- assets/script.js
+++ assets/script.js
@@ -40,3 +40,18 @@ $("#12time-display").text(timeDisplayEl);
 var current = moment().format('LT');
 // console.log(timeDisplayEl);
 
+var input = document.querySelector(".form-control");
+var inputText = document.querySelector("#input-value");
+var button = document.querySelector(".btn");
+var time = $(this).parent().attr('id');
+
+button.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("yes");
+    localStorage.setItem(time, input);
+
+});
+
+
+
+
