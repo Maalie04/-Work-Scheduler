ef2e07404c86ed213f8afd67b908fe8ab71d9d44
Jamaal Lawson <maaliemaal@gmail.com>
2021-06-25 12:42:50 -0400

    added text and the current time to my jumbotron, and added a object that holds my input value

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,45 @@
+var current = moment().hours();
+var today = document.querySelector("#currentDay");
+var textA9 = document.querySelector("#input-9");
+var textA10 = document.querySelector("#input-10");
+var textA11 = document.querySelector("#input-11");
+var textA12 = document.querySelector("#input-12");
+var textA13 = document.querySelector("#input-13");
+var textA14 = document.querySelector("#input-14");
+var textA15 = document.querySelector("#input-15");
+var textA16 = document.querySelector("#input-16");
 
-var current = moment().format('LT');
-// console.log(timeDisplayEl);
+today = moment().format("dddd MMMM Do");  
+$("#currentDay").text(today);
+
+console.log(today)
 
 var input = document.querySelector(".form-control");
 var inputText = document.querySelector("#input-value");
 var button = document.querySelector(".btn");
 var time = $(this).parent().attr('id');
 
+if(current) {
+    $('textarea').addClass('present');
+}
+
 button.addEventListener("click", function(event) {
     event.preventDefault();
     console.log("yes");
-    localStorage.setItem(time, input);
+    
+var user = {
+    input: textA9.value.trim(),
+    input: textA10.value.trim(),
+    input: textA11.value.trim(),
+    input: textA12.value.trim(),
+    input: textA13.value.trim(),
+    input: textA14.value.trim(),
+    input: textA15.value.trim(),
+    input: textA16.value.trim(),   
+};
+
+
+    localStorage.setItem(user, JSON.stringify(user));
 
 });
 
